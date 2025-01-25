cb62c3b29cf61cfd29a81936961d8658abff0361
Julien Vedrenne <julien.vedrenne@edu.ece.fr>
2025-01-25 19:24:51 +0100

    Update index.tsx to use ReactDOM.createRoot for rendering App component

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Home from './pages/Home';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
